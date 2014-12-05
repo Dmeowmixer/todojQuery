3a1aa7e092212888ebc0af80ab3753c69f4fad2d
dmeowmixer <dmeowmixer@hotmail.com>
2014-12-04 20:07:23 -1000

    got field to clear

--- public/js/todo.js
+++ public/js/todo.js
@@ -2,19 +2,11 @@ $(function(){
   $('.inputfield').keydown(function(e){
     console.log(e);
     if (e.keyCode == 13){
-      console.log(e.currentTarget);
+
       var inputvalue = $('.inputfield').val();
+
       $('ul').append('<li>'+inputvalue+'</li>');
-    function fillField(input,val){
-      if(input.val == ""){
-        input.val = input.val;
-      }
-    };
-    function clearField(input,val){
-      if(input.val == val){
-        input.val = "";
-      }
-    };
+      $(this).val("");
     }
   });
 });
