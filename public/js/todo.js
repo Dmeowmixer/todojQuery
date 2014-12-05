e6b807c8d71ae87fe872313fcb96639da0d29010
dmeowmixer <dmeowmixer@hotmail.com>
2014-12-04 21:59:19 -1000

    got linethrough to work individually

--- public/js/todo.js
+++ public/js/todo.js
@@ -7,7 +7,14 @@ $(function(){
 
       $('ul').append('<li>'+ '<input type="checkbox">'+inputvalue+'</li>');
       $(this).val("");
-
     }
   });
+  $('body').on("click",":checkbox",function(){
+    if (this.checked){
+      $(this).parent().css("text-decoration", "line-through");
+    }
+    else{
+      $(this).parent().css("text-decoration", "none");
+    }
+  })
 });
